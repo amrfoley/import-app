8295480ddff4bee77e127fc7b9362c183bb5c751
Amr Foley <amr.foley@chefaa.com>
2022-03-10 17:08:11 +0200

    Write ReadME file

--- routes/api.php
+++ routes/api.php
@@ -16,4 +16,4 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('article', 'ArticleController@show');
 Route::get('articles', 'ArticleController@index');
-Route::post('articles/import', 'ArticleController@import');
+Route::post('articles', 'ArticleController@import');
